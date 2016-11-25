53db921af34cc81f8036e95c2e8bbc438a2b4a3c
Dmitriy Ruzhiy <golovolomka132@gmail.com>
2016-11-25 18:05:55 +0200

    First Just adding files.

---
+++ app/src/main/java/com/example/dmitriy/schedulenew/fragments/mon.java
@@ -0,0 +1,21 @@
+package com.example.dmitriy.schedulenew.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.dmitriy.schedulenew.R;
+
+
+
+public class mon extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.mon, container,false);
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/example/dmitriy/schedulenew/fragments/thu.java
@@ -0,0 +1,19 @@
+package com.example.dmitriy.schedulenew.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dmitriy.schedulenew.R;
+
+
+public class thu extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.thu, container,false);
+        return rootView;
+    }
+
+}
